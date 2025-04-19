021a49a0d5c34afba6a27f568bc591a386e40cf0
emirjeremiah <emirjeremiahmarcelino@gmail.com>
2025-04-19 17:00:52 +0800

    folder structure partial
    de pa tapos. wala pa linking. lalo na mga images. de ko pa din na append

--- Register/patient.js
+++
@@ -1,9 +0,0 @@
-function editProfile() {
-  const name = prompt("Enter patient name:", document.getElementById("patient-name").textContent);
-  const age = prompt("Enter age:", document.getElementById("patient-age").textContent.replace("Age: ", ""));
-  const gender = prompt("Enter gender:", document.getElementById("patient-gender").textContent.replace("Gender: ", ""));
-
-  if (name) document.getElementById("patient-name").textContent = name;
-  if (age) document.getElementById("patient-age").textContent = "Age: " + age;
-  if (gender) document.getElementById("patient-gender").textContent = "Gender: " + gender;
-}
